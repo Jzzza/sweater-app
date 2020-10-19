d59420fb08b718ade7ee43436b8ff3c4244ab670
Jzzza <jzzza1@gmail.com>
2020-10-19 23:33:03 +0300

    Update template and controller (2)

--- src/main/java/ru/dmartyanov/greetings/controllers/GreetingController.java
+++ src/main/java/ru/dmartyanov/greetings/controllers/GreetingController.java
@@ -1,14 +1,18 @@
 package ru.dmartyanov.greetings.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.dmartyanov.greetings.domain.Message;
+import ru.dmartyanov.greetings.repos.MessageRepo;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepos;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",
@@ -21,7 +25,8 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some", "hello");
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
         return "main";
     }
 }
--- src/main/java/ru/dmartyanov/greetings/repos/MessageRepo.java
+++ src/main/java/ru/dmartyanov/greetings/repos/MessageRepo.java
@@ -3,5 +3,5 @@ package ru.dmartyanov.greetings.repos;
 import org.springframework.data.repository.CrudRepository;
 import ru.dmartyanov.greetings.domain.Message;
 
-public interface MessageRepos extends CrudRepository<Message, Long> {
+public interface MessageRepo extends CrudRepository<Message, Long> {
 }
