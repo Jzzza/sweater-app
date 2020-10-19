067e14ebb2154271c9cde5742279a6e3ead00dfd
Jzzza <jzzza1@gmail.com>
2020-10-19 23:56:11 +0300

    Add method for filtering result from repo (2)

--- src/main/java/ru/dmartyanov/greetings/repos/MessageRepo.java
+++ src/main/java/ru/dmartyanov/greetings/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package ru.dmartyanov.greetings.repos;
 import org.springframework.data.repository.CrudRepository;
 import ru.dmartyanov.greetings.domain.Message;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
