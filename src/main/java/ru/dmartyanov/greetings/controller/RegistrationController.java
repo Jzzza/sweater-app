da2bb4ad0b230e4a06c05e80a02c2d6dd245e837
Jzzza <jzzza1@gmail.com>
2020-10-21 03:03:48 +0300

    Update user add method (3)

--- src/main/java/ru/dmartyanov/greetings/controller/RegistrationController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/RegistrationController.java
@@ -1,20 +1,39 @@
 package ru.dmartyanov.greetings.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.dmartyanov.greetings.domain.Role;
 import ru.dmartyanov.greetings.domain.User;
+import ru.dmartyanov.greetings.repos.UserRepo;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user) {
+    public String addUser(User user,
+                          Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
 
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+
+        userRepo.save(user);
         return "redirect:/login";
     }
 }
