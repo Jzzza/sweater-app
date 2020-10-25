3afbbd6730c24537fdcd1bf5283ba2cb23114d55
Jzzza <jzzza1@gmail.com>
2020-10-25 06:20:33 +0300

    Added method for saving user (6)

--- src/main/java/ru/dmartyanov/greetings/controller/UserController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/UserController.java
@@ -3,12 +3,15 @@ package ru.dmartyanov.greetings.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.dmartyanov.greetings.domain.Role;
 import ru.dmartyanov.greetings.domain.User;
 import ru.dmartyanov.greetings.repos.UserRepo;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -28,5 +31,27 @@ public class UserController {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam("userId") User user,
+                           @RequestParam Map<String, String> form,
+                           @RequestParam String username){
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+        return "redirect:/user";
     }
 }
