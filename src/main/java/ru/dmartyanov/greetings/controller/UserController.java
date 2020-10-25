dff6e4a2d7dd71f9468d0fa2033b31445f4f3d24
Jzzza <jzzza1@gmail.com>
2020-10-25 05:37:25 +0300

    Added showing all users from repo (6)

--- src/main/java/ru/dmartyanov/greetings/controller/UserController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.dmartyanov.greetings.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.dmartyanov.greetings.repos.UserRepo;
@@ -13,7 +14,8 @@ public class UserController {
     private UserRepo userRepo;
 
     @GetMapping
-    public String userList() {
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
         return "userList";
     }
 }
