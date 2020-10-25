57f1369dae7b640a05a1e6a79ace2e80425bb478
Jzzza <jzzza1@gmail.com>
2020-10-25 19:45:31 +0300

    Change for windows and fix small problems (7)

--- src/main/java/ru/dmartyanov/greetings/controller/MainController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/MainController.java
@@ -67,7 +67,7 @@ public class MainController {
             String uuidFile = UUID.randomUUID().toString();
             String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
-            file.transferTo(new File(resultFilename));
+            file.transferTo(new File(uploadPath + "\\" + resultFilename));
 
             message.setFilename(resultFilename);
         }
