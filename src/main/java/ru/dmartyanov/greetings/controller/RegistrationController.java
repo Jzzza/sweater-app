279e3794847dfd348d2b1f1b2550ed3be4e1c9e2
Jzzza <jzzza1@gmail.com>
2020-10-27 23:43:07 +0300

    Some refactoring user services (9)

--- src/main/java/ru/dmartyanov/greetings/controller/RegistrationController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/RegistrationController.java
@@ -4,17 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.dmartyanov.greetings.domain.Role;
 import ru.dmartyanov.greetings.domain.User;
-import ru.dmartyanov.greetings.repos.UserRepo;
+import ru.dmartyanov.greetings.service.UserService;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -24,16 +22,11 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(User user,
                           Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null) {
+        if (!userService.addUser(user)) {
             model.put("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-
-        userRepo.save(user);
         return "redirect:/login";
     }
 }
--- src/main/java/ru/dmartyanov/greetings/service/UserService.java
+++ src/main/java/ru/dmartyanov/greetings/service/UserService.java
@@ -5,7 +5,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import ru.dmartyanov.greetings.domain.Role;
+import ru.dmartyanov.greetings.domain.User;
 import ru.dmartyanov.greetings.repos.UserRepo;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,5 +23,24 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())){
+
+        }
+
+        return true;
     }
 }
