0e3d2db9f3548c9f110ff11c0c72d3903742a8c8
Jzzza <jzzza1@gmail.com>
2020-11-03 00:49:21 +0300

    Splited and updated configuration (13)

--- src/main/java/ru/dmartyanov/greetings/config/WebSecurityConfig.java
+++ src/main/java/ru/dmartyanov/greetings/config/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package ru.dmartyanov.greetings.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.dmartyanov.greetings.service.UserService;
 
@@ -22,10 +20,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new MessageDigestPasswordEncoder("MD5");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/ru/dmartyanov/greetings/controller/MainController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/MainController.java
@@ -74,7 +74,7 @@ public class MainController {
                 String uuidFile = UUID.randomUUID().toString();
                 String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
-                file.transferTo(new File(uploadPath + "\\" + resultFilename));
+                file.transferTo(new File(uploadPath + "/" + resultFilename));
 
                 message.setFilename(resultFilename);
             }
