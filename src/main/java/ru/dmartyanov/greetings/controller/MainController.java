2587fa573cfcbb6f0ecd616d81e621252bd30323
Jzzza <jzzza1@gmail.com>
2020-11-05 22:22:07 +0300

    Added base for working with subscriptions (16)

--- src/main/java/ru/dmartyanov/greetings/controller/MainController.java
+++ src/main/java/ru/dmartyanov/greetings/controller/MainController.java
@@ -105,6 +105,9 @@ public class MainController {
         Set<Message> messages = user.getMessages();
         model.addAttribute("messages", messages);
         model.addAttribute("message", message);
+        model.addAttribute("userChannel", user);
+        model.addAttribute("subscriptionsCount", user.getSubscriptions().size());
+        model.addAttribute("subscribersCount", user.getSubscribers().size());
         model.addAttribute("isCurrentUser", currentUser.equals(user));
         return "userMessages";
     }
